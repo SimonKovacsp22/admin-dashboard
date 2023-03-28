36a3bd46ec012ea1b150aebc29e7ebc0f2162b6e
SimonKovacsp22 <postcardsent@gmail.com>
2023-03-28 09:42:27 +0200

    server setup with mongo + product, productStat and user routes

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRouter from "./routes/client.js";
+import managementRouter from "./routes/management.js";
+import generalRouter from "./routes/general.js";
+import salesRouter from "./routes/sales.js";
+
+//data imports
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import { dataProduct, dataProductStat } from "./data/index.js";
+import ProductStat from "./models/ProductStat.js";
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/client", clientRouter);
+app.use("/management", managementRouter);
+app.use("/general", generalRouter);
+app.use("/sales", salesRouter);
+
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server listening on Port: ${PORT}`));
+  })
+  .catch((error) => console.log(error));
