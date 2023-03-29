028e492aa3dc6ad6e6d42b2d174deed51435f132
SimonKovacsp22 <postcardsent@gmail.com>
2023-03-29 23:13:34 +0200

    added aggrogate function not working yet, overall stats and affiliate stats, transtactions, and admin route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import listEndpoints from "express-list-endpoints";
 import clientRouter from "./routes/client.js";
 import managementRouter from "./routes/management.js";
 import generalRouter from "./routes/general.js";
@@ -12,8 +13,17 @@ import salesRouter from "./routes/sales.js";
 //data imports
 import User from "./models/User.js";
 import Product from "./models/Product.js";
-import { dataProduct, dataProductStat } from "./data/index.js";
+import {
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
 import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
 const app = express();
 app.use(express.json());
@@ -38,5 +48,6 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server listening on Port: ${PORT}`));
+    console.table(listEndpoints(app));
   })
   .catch((error) => console.log(error));
